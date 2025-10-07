59b4ce245b63f9a763efa92cb4bf0a0210ac98ef
Ivan <sheva134@mail.ru>
2025-10-08 02:57:19 +0300

    Выполение задания

---
+++ tests.py
@@ -0,0 +1,95 @@
+import pytest
+from main import BooksCollector
+
+
+class TestBooksCollector:
+
+    def test_add_new_book_add_two_books(self):
+       
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        assert len(collector.get_books_genre()) == 2
+
+    def test_add_new_book_book_has_empty_genre_by_default(self):
+        
+        collector = BooksCollector()
+        collector.add_new_book('Новая книга')
+        assert collector.get_book_genre('Новая книга') == ''
+
+    def test_add_new_book_does_not_add_book_with_name_longer_than_40_chars(self):
+    
+        collector = BooksCollector()
+        long_name = 'A' * 41
+        collector.add_new_book(long_name)
+        assert long_name not in collector.get_books_genre()
+
+    def test_add_new_book_does_not_add_duplicate_book(self):
+        
+        collector = BooksCollector()
+        collector.add_new_book('Дубль')
+        collector.add_new_book('Дубль')
+        assert len(collector.get_books_genre()) == 1
+
+    @pytest.mark.parametrize("book_title, genre", [
+        ("Дюна", "Фантастика"),
+        ("Оно", "Ужасы"),
+        ("Шерлок", "Детективы"),
+        ("Тачки", "Мультфильмы"),
+        ("Операция Ы", "Комедии")
+    ])
+    def test_set_and_get_book_genre_valid_genres(self, book_title, genre):
+        
+        collector = BooksCollector()
+        collector.add_new_book(book_title)
+        collector.set_book_genre(book_title, genre)
+        assert collector.get_book_genre(book_title) == genre
+
+    def test_set_book_genre_ignores_invalid_genre(self):
+        
+        collector = BooksCollector()
+        collector.add_new_book('Тайна')
+        collector.set_book_genre('Тайна', 'Биография')  # такого жанра нет
+        assert collector.get_book_genre('Тайна') == ''
+
+    def test_get_books_with_specific_genre_returns_correct_books(self):
+       
+        collector = BooksCollector()
+        collector.add_new_book('Звёзды')
+        collector.add_new_book('Смех')
+        collector.set_book_genre('Звёзды', 'Фантастика')
+        collector.set_book_genre('Смех', 'Комедии')
+        assert collector.get_books_with_specific_genre('Фантастика') == ['Звёзды']
+
+    def test_get_books_for_children_excludes_age_rating_genres(self):
+        
+        collector = BooksCollector()
+        
+        for genre in collector.genre:
+            book = f"Книга {genre}"
+            collector.add_new_book(book)
+            collector.set_book_genre(book, genre)
+
+        children_books = collector.get_books_for_children()
+        age_rating_books = ["Книга Ужасы", "Книга Детективы"]
+        non_age_books = ["Книга Фантастика", "Книга Мультфильмы", "Книга Комедии"]
+
+        for book in age_rating_books:
+            assert book not in children_books
+        for book in non_age_books:
+            assert book in children_books
+
+    def test_add_book_in_favorites_adds_existing_book(self):
+        
+        collector = BooksCollector()
+        collector.add_new_book('Любимая')
+        collector.add_book_in_favorites('Любимая')
+        assert 'Любимая' in collector.get_list_of_favorites_books()
+
+    def test_delete_book_from_favorites_removes_book(self):
+        
+        collector = BooksCollector()
+        collector.add_new_book('Удаляемая')
+        collector.add_book_in_favorites('Удаляемая')
+        collector.delete_book_from_favorites('Удаляемая')
+        assert 'Удаляемая' not in collector.get_list_of_favorites_books()
