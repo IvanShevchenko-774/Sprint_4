af4c97cbf96d823c07f3814182bde4c7a5f4cb78
Ivan <sheva134@mail.ru>
2025-10-15 09:19:40 +0300

    Исправление отступов

--- tests.py
+++ tests.py
@@ -5,27 +5,23 @@ from main import BooksCollector
 class TestBooksCollector:
 
     def test_add_new_book_add_two_books(self):
-       
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         assert len(collector.get_books_genre()) == 2
 
     def test_add_new_book_book_has_empty_genre_by_default(self):
-        
         collector = BooksCollector()
         collector.add_new_book('Новая книга')
         assert collector.get_book_genre('Новая книга') == ''
 
     def test_add_new_book_does_not_add_book_with_name_longer_than_40_chars(self):
-    
         collector = BooksCollector()
         long_name = 'A' * 41
         collector.add_new_book(long_name)
         assert long_name not in collector.get_books_genre()
 
     def test_add_new_book_does_not_add_duplicate_book(self):
-        
         collector = BooksCollector()
         collector.add_new_book('Дубль')
         collector.add_new_book('Дубль')
@@ -52,14 +48,12 @@ class TestBooksCollector:
         assert book_name not in collector.get_books_genre()
 
     def test_set_book_genre_ignores_invalid_genre(self):
-        
         collector = BooksCollector()
         collector.add_new_book('Тайна')
         collector.set_book_genre('Тайна', 'Биография')  # такого жанра нет
         assert collector.get_book_genre('Тайна') == ''
 
     def test_get_books_with_specific_genre_returns_correct_books(self):
-       
         collector = BooksCollector()
         collector.add_new_book('Звёзды')
         collector.add_new_book('Смех')
@@ -80,14 +74,12 @@ class TestBooksCollector:
         assert 'Тачки' in collector.get_books_for_children()
 
     def test_add_book_in_favorites_adds_existing_book(self):
-        
         collector = BooksCollector()
         collector.add_new_book('Любимая')
         collector.add_book_in_favorites('Любимая')
         assert 'Любимая' in collector.get_list_of_favorites_books()
 
     def test_delete_book_from_favorites_removes_book(self):
-        
         collector = BooksCollector()
         collector.add_new_book('Удаляемая')
         collector.add_book_in_favorites('Удаляемая')
